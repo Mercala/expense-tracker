c84032655dbb6bef23a3efb75fabfbe02f509c30
Angelo Willems <stochastictraders@gmail.com>
2025-05-31 14:40:48 -0400

    Added version as variable

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ from PyQt6.QtWidgets import (QApplication, QMainWindow,
 
 import warnings
 from PyQt6.QtCore import pyqtRemoveInputHook
+from .version import __version__
 
 warnings.filterwarnings("ignore", category=DeprecationWarning, message=".*sipPyTypeDict().*")
 
@@ -12,7 +13,7 @@ warnings.filterwarnings("ignore", category=DeprecationWarning, message=".*sipPyT
 class ExpenseTracker(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.version = "1.0.1"  # Will be checked against GitHub releases
+        self.version = __version__
         self.init_ui()
 
     def init_ui(self):
